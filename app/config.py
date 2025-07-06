355a578411ea38ce0a1df25e458ef31bdb874372
MAT3514_CTDLGT <phanvanthanh_t66@hus.edu.vn>
2025-07-06 14:02:12 +0700

    ...

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 DATA_DIR = "law_data"
 FAISS_PATH = "faiss_index"
